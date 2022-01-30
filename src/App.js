a7a8f0d13978b2dc1d7bb7efa07789d8e240e7e4
evsem <frukwork@gmail.com>
2022-01-30 13:35:29 +0300

    v_7_Sort

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react'
 import './App.css'
 import PostForm from './Components/PostForm/PostForm'
 import PostList from './Components/PostList/PostList'
+import MySelect from './UI/Select/MySelect'
 
 function App() {
   let [posts, setPosts] = useState([
     { id: 1, title: 'JS', body: 'Programming language' },
   ])
+  let [selectedSort, setSelectedSort] = useState('')
 
   const addNewPost_func = (newPost) => {
     setPosts([...posts, newPost])
@@ -15,10 +17,27 @@ function App() {
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id))
   }
+
+  const sortPosts = (sort) => {
+    //Перезаписвыаем состояние
+    setSelectedSort(sort)
+    //Передаём отсортированный массив; функция sort не возвращает новый отсортированный массив, она мутирует с изначальным(для этого массив постов деструктуризируем и разворачиваем); localeCompare - функция сравнения строк.
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
   return (
     <div className="App">
       <div className="main_wrapper">
         <PostForm func_forAddNewPost={addNewPost_func} />
+        <hr style={{ margin: '10px 0px' }} />
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Sortirovka"
+          options={[
+            { value: 'title', name: 'By name' },
+            { value: 'body', name: 'By discription' },
+          ]}
+        />
         {posts.length !== 0 ? (
           <PostList props_postList={posts} remove={removePost} />
         ) : (
